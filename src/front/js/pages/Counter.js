2690c1f736ca2572e7104f457c3df9c63f5b4406
Doctorine06 <fergtheartist@gmail.com>
2023-08-19 16:46:23 +0000

    pop

--- src/front/js/pages/Counter.js
+++ src/front/js/pages/Counter.js
@@ -47,7 +47,6 @@ const Counter = () => {
             <input placeholder="Reloj" size="115;"></input>
             <button>
               {" "}
-              <Clock sec={seconds} />, document.querySelector("#clock")
               <i class="fa-sharp fa-light fa-user-clock fa-shake"></i>Start The
               Clock
             </button>
@@ -68,6 +67,35 @@ const Counter = () => {
         <div>
           {" "}
           <ol>
+            <li>
+              <iframe
+                width="100%"
+                height="300"
+                scrolling="no"
+                frameborder="no"
+                allow="autoplay"
+                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1498308427&color=%238000ff&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+              ></iframe>
+              <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
+                <a
+                  href="https://soundcloud.com/user-647680600"
+                  title="Jeffery"
+                  target="_blank"
+                  style="color: #cccccc; text-decoration: none;"
+                >
+                  Jeffery
+                </a>{" "}
+                ·{" "}
+                <a
+                  href="https://soundcloud.com/user-647680600/emastered_please-father-1"
+                  title="eMastered_Please Father"
+                  target="_blank"
+                  style="color: #cccccc; text-decoration: none;"
+                >
+                  eMastered_Please Father
+                </a>
+              </div>
+            </li>
             <li>
               <iframe
                 width="560"

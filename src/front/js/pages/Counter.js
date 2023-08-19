f91ec96728594170f969ea078a845b982c60ab9d
Doctorine06 <fergtheartist@gmail.com>
2023-08-19 17:02:44 +0000

    troubleshooting components .net

--- src/front/js/pages/Counter.js
+++ src/front/js/pages/Counter.js
@@ -69,35 +69,6 @@ const Counter = () => {
           <ol>
             <li>
               <iframe
-                width="100%"
-                height="300"
-                scrolling="no"
-                frameborder="no"
-                allow="autoplay"
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1498308427&color=%238000ff&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-              ></iframe>
-              <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
-                <a
-                  href="https://soundcloud.com/user-647680600"
-                  title="Jeffery"
-                  target="_blank"
-                  style="color: #cccccc; text-decoration: none;"
-                >
-                  Jeffery
-                </a>{" "}
-                ·{" "}
-                <a
-                  href="https://soundcloud.com/user-647680600/emastered_please-father-1"
-                  title="eMastered_Please Father"
-                  target="_blank"
-                  style="color: #cccccc; text-decoration: none;"
-                >
-                  eMastered_Please Father
-                </a>
-              </div>
-            </li>
-            <li>
-              <iframe
                 width="560"
                 height="202"
                 src="https://www.bandlab.com/embed/?id=a24f825a-3c22-ee11-a9bb-000d3a41e8b8"
@@ -143,6 +114,34 @@ const Counter = () => {
         <hr />
         Please do not share your personal information with others
         <hr />
+        <iframe
+          width="100%"
+          height="300"
+          scrolling="no"
+          frameborder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1498308427&color=%238000ff&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        ></iframe>
+        <div>
+          {/* <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"> */}
+          <a
+            href="https://soundcloud.com/user-647680600"
+            title="Jeffery"
+            target="_blank"
+            // style="color: #cccccc; text-decoration: none;"
+          >
+            Jeffery
+          </a>{" "}
+          ·{" "}
+          <a
+            href="https://soundcloud.com/user-647680600/emastered_please-father-1"
+            title="eMastered_Please Father"
+            target="_blank"
+            // style="color: #cccccc; text-decoration: none;"
+          >
+            eMastered_Please Father
+          </a>
+        </div>
       </div>
     </div>
   );
